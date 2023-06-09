26aab4a860bf1b7ffb1b797c6e3cfbed827cbac4
Faraz <farazzkamali80@gmail.com>
2023-06-09 19:15:14 +0330

    feat: display critic score

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,7 +1,7 @@
-import { HStack, Icon, Text } from "@chakra-ui/react"
+import { HStack, Icon } from "@chakra-ui/react"
 import { Platform } from "../hooks/useGames"
 import {FaWindows, FaPlaystation, FaApple, FaXbox, FaLinux, FaAndroid} from 'react-icons/fa'
-import {MdPhoneIphone} from 'react-icons/md'    
+
 import {SiNintendo} from 'react-icons/si'
 import {BsGlobe} from 'react-icons/bs'
 import { IconType } from "react-icons"
