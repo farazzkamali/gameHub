1a0d0ea43a992c9a53ba90f1a8bed14231a5c3ff
Faraz <farazzkamali80@gmail.com>
2023-07-12 17:23:29 +0330

    feat: Build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,9 +1,9 @@
+import { Heading, Spinner } from "@chakra-ui/react"
 import { useParams } from "react-router-dom"
-import useGame from "../hooks/useGame"
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react"
 import ExpandableText from "../components/ExpandableText"
-import DefenitionItem from "../components/DefinitionItem"
-import CriticScore from "../components/CriticScore"
+import GameAttributes from "../components/GameAttributes"
+import useGame from "../hooks/useGame"
+import GameTrailer from "../components/GameTrailer"
 
 
 const GameDetailPage = () => {
@@ -17,24 +17,9 @@ const GameDetailPage = () => {
         <>
         <Heading>{game.name}</Heading>
         <ExpandableText>{game.description_raw}</ExpandableText>
+        <GameAttributes game={game}/>
+        <GameTrailer gameId={game.id}/>
 
-        <SimpleGrid columns={2} as='dl'>
-            <DefenitionItem term="Platforms">
-                {game.parent_platforms?.map(p=>
-                    <Text>{p.platform.name}</Text>
-                )}
-                {/* destructured: {game.parent_platforms?.map(({platform})=><Text>{platform.name}</Text>)} */}
-            </DefenitionItem>
-            <DefenitionItem term="Metascore">
-                <CriticScore score={game.metacritic}/>
-            </DefenitionItem>
-            <DefenitionItem term="Genres">
-                {game.genres.map(genre=><Text key={genre.id}>{genre.name}</Text>)}
-            </DefenitionItem>
-            <DefenitionItem term="Publishers">
-                {game.publishers?.map(publisher=><Text key={publisher.id}>{publisher.name}</Text>)}
-            </DefenitionItem>
-        </SimpleGrid>
         </>
     )
 }
