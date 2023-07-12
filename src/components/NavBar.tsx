50472af327e5d5a3009c2cd352fc0b411de129ac
Faraz <farazzkamali80@gmail.com>
2023-07-12 17:52:52 +0330

    fix: nav search & logo bug

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,15 @@
 import { HStack, Image } from '@chakra-ui/react'
 import ColorModeSwitch from './ColorModeSwitch'
 import SearchInput from './SearchInput'
+import { Link } from 'react-router-dom'
 
 
 const NavBar = () => { 
     return (
         <HStack padding='10px'>
-            <Image src="https://via.placeholder.com/150" alt="logo" boxSize='60px' />
+            <Link to='/'>
+                <Image src="https://via.placeholder.com/150" alt="logo" boxSize='60px' objectFit='cover'/>
+            </Link>
             <SearchInput/>
             <ColorModeSwitch />
         </HStack>
